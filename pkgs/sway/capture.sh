9dfa71a372f6d31f8b487c18176cd66e008ed41b
sweenu <brunoinec@gmail.com>
2022-10-17 18:43:05 +0200

    pkgs: improve scripts

--- pkgs/sway/capture.sh
+++ pkgs/sway/capture.sh
@@ -7,40 +7,42 @@ notify() {
 }
 
 screenshot() {
-    grim $1
+    grim "$1"
 }
 
 slurped_screenshot() {
-    grim -g "$(slurp)" $1
+    grim -g "$(slurp)" "$1"
 }
 
 modifiable_screenshot() {
-    grim -g "$(slurp)" - | swappy -f - -o $1
+    grim -g "$(slurp)" - | swappy -f - -o "$1"
 }
 
 screencast() {
-    wf-recorder -g "$(slurp)" -f $1
+    wf-recorder -g "$(slurp)" -f "$1"
 }
 
 main() {
     while getopts "o:" opt; do
-       case $opt in
-          o) output_dir=$OPTARG;;
+       case "$opt" in
+          o) output_dir="$OPTARG";;
+          \?) echo unknown flag "$opt"
+             exit 1 ;;
        esac
     done
     shift $((OPTIND-1))
-    type=$1
+    type="$1"
     [ -z "$output_dir" ] && output_dir="."
 
     filepath="$output_dir/$(date '+screenshot_%Y-%m-%d-%H%M%S.png')"
     case "$type" in
-        screenshot|"") screenshot $filepath;;
-        slurped-screenshot) slurped_screenshot $filepath ;;
-        modifiable-screenshot) modifiable_screenshot $filepath ;;
+        screenshot|"") screenshot "$filepath";;
+        slurped-screenshot) slurped_screenshot "$filepath" ;;
+        modifiable-screenshot) modifiable_screenshot "$filepath" ;;
         screencast)
             filepath="$output_dir/$(date '+screencast_%Y-%m-%d-%H%M%S.mp4')";
-            if screencast $filepath; then
-                notify "Screencast saved as\n$(realpath $filepath)"
+            if screencast "$filepath"; then
+                notify "Screencast saved as\n$(realpath "$filepath")"
                 exit 0
             else
                 notify "Screencast failed"
@@ -51,8 +53,9 @@ main() {
             exit 1 ;;
     esac
 
+    # shellcheck disable=SC2181
     if [ "$?" -eq 0 ]; then
-        notify "Screenshot saved as\n$(realpath $filepath)"
+        notify "Screenshot saved as\n$(realpath "$filepath")"
     else
         notify "Screenshot failed"
     fi
