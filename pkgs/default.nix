aaacee76e99e086aef9eed7510507bbdb962daf6
sweenu <brunoinec@gmail.com>
2022-10-17 18:43:05 +0200

    pkgs: improve scripts

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,30 @@
 final: prev: with prev;
 {
-  fedit = writeShellScriptBin "fedit" (builtins.readFile ./fedit.sh);
+  fedit = writeShellApplication {
+    name = "fedit";
+    text = (builtins.readFile ./fedit.sh);
+    runtimeInputs = [ fd skim bat git ];
+  };
+
   kakounePlugins = kakounePlugins // recurseIntoAttrs (callPackage ./kakoune_plugins.nix { });
 
   # Used with sway keybindings
   sway-soundcards = writers.writeBashBin "sway-soundcards" (builtins.readFile ./sway/soundcards.bash);
   sway-app-or-workspace = writeShellScriptBin "sway-app-or-workspace" (builtins.readFile ./sway/app_or_workspace.sh);
-  sway-inhibit = writeShellScriptBin "sway-inhibit" (builtins.readFile ./sway/inhibit.sh);
   sway-backlight = writers.writePython3Bin "sway-backlight" { } (builtins.readFile ./sway/backlight.py);
-  sway-capture = writeShellScriptBin "sway-capture" (builtins.readFile ./sway/capture.sh);
-  sway-choose-capture = writeShellScriptBin "sway-choose-capture" (builtins.readFile ./sway/choose-capture.sh);
+  sway-inhibit = writeShellApplication {
+    name = "sway-inhibit";
+    text = (builtins.readFile ./sway/inhibit.sh);
+    runtimeInputs = [ libnotify ];
+  };
+  sway-capture = writeShellApplication {
+    name = "sway-capture";
+    text = (builtins.readFile ./sway/capture.sh);
+    runtimeInputs = [ grim slurp swappy wf-recorder libnotify ];
+  };
+  sway-choose-capture = writeShellApplication {
+    name = "sway-choose-capture";
+    text = (builtins.readFile ./sway/choose-capture.sh);
+    runtimeInputs = [ wofi ];
+  };
 }
