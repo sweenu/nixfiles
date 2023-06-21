5f85b504715b43d85a874535e61acc4a7126ca7f
Bruno Inec <contact@sweenu.xyz>
2023-06-21 14:25:24 +0200

    ig-story-fetcher: add postStop

--- hosts/najdorf/ig-story-fetcher.nix
+++ hosts/najdorf/ig-story-fetcher.nix
@@ -1,4 +1,4 @@
-{ self, config, ... }:
+{ self, config, pkgs, ... }:
 
 {
   age.secrets.IGStoryFetcher.file = "${self}/secrets/ig_story_fetcher_config.age";
@@ -9,5 +9,6 @@
     randomizedDelaySec = "1h";
     persistent = true;
     settingsPath = config.age.secrets.IGStoryFetcher.path;
+    postStop = "${pkgs.curl}/bin/curl https://hc-ping.com/df3a7e5d-5c19-4fbb-9065-ccf372855efa";
   };
 }
