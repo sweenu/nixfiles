6480cc4fbea99edd58bfba35538acb5e3990b1c7
Bruno Inec <contact@sweenu.xyz>
2024-05-20 18:46:14 +0200

    nextcloud: update 28 -> 29

--- hosts/najdorf/nextcloud.nix
+++ hosts/najdorf/nextcloud.nix
@@ -1,4 +1,10 @@
-{ self, config, pkgs, lib, ... }:
+{
+  self,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
   nextcloudDir = "/opt/nextcloud";
@@ -21,71 +27,78 @@ in
     "nextcloud/dbPassword".file = "${self}/secrets/nextcloud/db_password.age";
   };
 
-  virtualisation.arion.projects.nextcloud.settings = let version = "28"; in {
-    networks.traefik.external = true;
+  virtualisation.arion.projects.nextcloud.settings =
+    let
+      version = "29";
+    in
+    {
+      networks.traefik.external = true;
 
-    services = rec {
-      nextcloud.service = {
-        image = "nextcloud:${version}";
-        container_name = "nextcloud";
-        depends_on = [ "db" ];
-        networks = [ "default" config.virtualisation.arion.projects.traefik.settings.networks.traefik.name ];
-        volumes = nextcloudVolumes;
-        environment = {
-          POSTGRES_HOST = "db";
-          POSTGRES_DB = "nextcloud";
-          POSTGRES_USER = db.service.environment."POSTGRES_USER";
-          POSTGRES_PASSWORD_FILE = db.service.environment."POSTGRES_PASSWORD_FILE";
-          NEXTCLOUD_ADMIN_USER_FILE = config.age.secrets."nextcloud/adminUser".path;
-          NEXTCLOUD_ADMIN_PASSWORD_FILE = config.age.secrets."nextcloud/adminPassword".path;
-          NEXTCLOUD_TRUSTED_DOMAINS = "nextcloud.${config.vars.domainName}";
-          TRUSTED_PROXIES = "172.16.0.0/12";
-          OVERWRITEPROTOCOL = "https";
-          SMTP_HOST = config.vars.smtpHost;
-          SMTP_PORT = config.vars.smtpPort;
-          SMTP_NAME = config.vars.smtpUsername;
-          SMTP_SECURE = "tls";
-          MAIL_FROM_ADDRESS = config.vars.email;
-          MAIL_DOMAIN = builtins.elemAt (lib.strings.splitString "@" config.vars.email) 1;
-          PHP_UPLOAD_LIMIT = "16G";
-          APACHE_BODY_LIMIT = "0";
+      services = rec {
+        nextcloud.service = {
+          image = "nextcloud:${version}";
+          container_name = "nextcloud";
+          depends_on = [ "db" ];
+          networks = [
+            "default"
+            config.virtualisation.arion.projects.traefik.settings.networks.traefik.name
+          ];
+          volumes = nextcloudVolumes;
+          environment = {
+            POSTGRES_HOST = "db";
+            POSTGRES_DB = "nextcloud";
+            POSTGRES_USER = db.service.environment."POSTGRES_USER";
+            POSTGRES_PASSWORD_FILE = db.service.environment."POSTGRES_PASSWORD_FILE";
+            NEXTCLOUD_ADMIN_USER_FILE = config.age.secrets."nextcloud/adminUser".path;
+            NEXTCLOUD_ADMIN_PASSWORD_FILE = config.age.secrets."nextcloud/adminPassword".path;
+            NEXTCLOUD_TRUSTED_DOMAINS = "nextcloud.${config.vars.domainName}";
+            TRUSTED_PROXIES = "172.16.0.0/12";
+            OVERWRITEPROTOCOL = "https";
+            SMTP_HOST = config.vars.smtpHost;
+            SMTP_PORT = config.vars.smtpPort;
+            SMTP_NAME = config.vars.smtpUsername;
+            SMTP_SECURE = "tls";
+            MAIL_FROM_ADDRESS = config.vars.email;
+            MAIL_DOMAIN = builtins.elemAt (lib.strings.splitString "@" config.vars.email) 1;
+            PHP_UPLOAD_LIMIT = "16G";
+            APACHE_BODY_LIMIT = "0";
+          };
+          env_file = [ config.age.secrets."nextcloud/envFile".path ];
+          labels = {
+            "traefik.enable" = "true";
+            "traefik.http.routers.to-nextcloud.middlewares" = "nextcloud-redirectregex";
+            "traefik.http.middlewares.nextcloud-redirectregex.redirectregex.permanent" = "true";
+            "traefik.http.middlewares.nextcloud-redirectregex.redirectregex.regex" = "https://(.*)/.well-known/(card|cal)dav";
+            "traefik.http.middlewares.nextcloud-redirectregex.redirectregex.replacement" = "https://\$\${1}/remote.php/dav/";
+          };
         };
-        env_file = [ config.age.secrets."nextcloud/envFile".path ];
-        labels = {
-          "traefik.enable" = "true";
-          "traefik.http.routers.to-nextcloud.middlewares" = "nextcloud-redirectregex";
-          "traefik.http.middlewares.nextcloud-redirectregex.redirectregex.permanent" = "true";
-          "traefik.http.middlewares.nextcloud-redirectregex.redirectregex.regex" = "https://(.*)/.well-known/(card|cal)dav";
-          "traefik.http.middlewares.nextcloud-redirectregex.redirectregex.replacement" = "https://\$\${1}/remote.php/dav/";
+
+        db.service = {
+          image = "postgres:11";
+          container_name = "nextcloud_db";
+          networks = [ "default" ];
+          environment = {
+            POSTGRES_USER = "postgres";
+            POSTGRES_PASSWORD_FILE = config.age.secrets."nextcloud/dbPassword".path;
+          };
+          volumes = [
+            "${nextcloudDir}/pg_data:/var/lib/postgresql/data"
+            "${nextcloudSecretsDir}:${nextcloudSecretsDir}:ro"
+          ];
+        };
+
+        cron.service = {
+          image = "nextcloud:${version}";
+          volumes = nextcloudVolumes;
+          entrypoint = "/cron.sh";
+          depends_on = [ "db" ];
         };
       };
 
-      db.service = {
-        image = "postgres:11";
-        container_name = "nextcloud_db";
-        networks = [ "default" ];
-        environment = {
-          POSTGRES_USER = "postgres";
-          POSTGRES_PASSWORD_FILE = config.age.secrets."nextcloud/dbPassword".path;
-        };
-        volumes = [
-          "${nextcloudDir}/pg_data:/var/lib/postgresql/data"
-          "${nextcloudSecretsDir}:${nextcloudSecretsDir}:ro"
-        ];
-      };
-
-      cron.service = {
-        image = "nextcloud:${version}";
-        volumes = nextcloudVolumes;
-        entrypoint = "/cron.sh";
-        depends_on = [ "db" ];
+      docker-compose.volumes = {
+        nextcloud = { };
       };
     };
-
-    docker-compose.volumes = {
-      nextcloud = { };
-    };
-  };
 
   services.restic.backups.opt.backupPrepareCommand =
     let
