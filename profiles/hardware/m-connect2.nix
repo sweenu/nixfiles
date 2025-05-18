9df7039fd3c846ef414e9e4f7ff5bd261237096c
Bruno Inec <contact@sweenu.xyz>
2025-05-18 16:20:15 +0200

    Revert "support m/connect 2"
    This reverts commit 2350afeb4d20958b8b473ad9a3279517942220c2.

--- profiles/hardware/m-connect2.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-
-{
-  services.xserver.videoDrivers = [ "displaylink" ];
-  systemd.services.dlm.wantedBy = [ "multi-user.target" ];
-  environment.variables = {
-    WLR_EVDI_RENDER_DEVICE = "/dev/dri/card1";
-  };
-}
