1204212e878a4c3f4df6dbb27ca78c32e5b5e4de
sweenu <contact@sweenu.xyz>
2025-08-31 17:36:04 +0200

    vars: change font variables

--- profiles/graphical/base.nix
+++ profiles/graphical/base.nix
@@ -4,14 +4,6 @@
   boot.extraModulePackages = [ config.boot.kernelPackages.ddcci-driver ];
   boot.kernelModules = [ "ddcci" ];
 
-  fonts.packages = with pkgs; [
-    roboto
-    font-awesome
-    nerd-fonts.dejavu-sans-mono
-    twitter-color-emoji
-    material-symbols
-  ];
-
   programs = {
     kdeconnect.enable = true;
     thunar.enable = true;
