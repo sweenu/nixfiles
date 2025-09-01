c4511eb3f51a017c7634093a2338306535184e0b
sweenu <contact@sweenu.xyz>
2025-09-01 14:15:01 +0200

    wireplumber: disable automatic headset switching

--- profiles/audio.nix
+++ profiles/audio.nix
@@ -3,5 +3,10 @@
     enable = true;
     alsa.enable = true;
     pulse.enable = true;
+    wireplumber.extraConfig."11-bluetooth-policy" = {
+      "wireplumber.settings" = {
+        "bluetooth.autoswitch-to-headset-profile" = false;
+      };
+    };
   };
 }
