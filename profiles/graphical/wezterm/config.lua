e66944d7771eafa6754965eab009116fd187a200
sweenu <contact@sweenu.xyz>
2025-09-03 11:33:42 +0200

    caelestia: make dynamic theme work better

--- profiles/graphical/wezterm/config.lua
+++ profiles/graphical/wezterm/config.lua
@@ -6,14 +6,15 @@ config.term = 'wezterm'
 
 -- Appearance
 config.enable_tab_bar = false
-config.window_background_opacity = 1.0
+config.window_background_opacity = 0.78
 config.window_content_alignment = {
   horizontal = 'Center',
   vertical = 'Center',
 }
 config.window_decorations = "NONE"
 config.font_size = 10.0
-config.color_scheme = 'OceanicNext (base16)'
+-- Use dynamic themeing from Caelestia
+-- config.color_scheme = 'OceanicNext (base16)'
 
 -- Key bindings
 config.disable_default_key_bindings = true
