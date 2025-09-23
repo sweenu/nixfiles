2d3fa5db5bf0b275ad701f08efa02a3450777530
sweenu <contact@sweenu.xyz>
2025-09-23 03:02:21 +0200

    najdorf(n8n): 1.94 -> 1.111

--- hosts/najdorf/n8n.nix
+++ hosts/najdorf/n8n.nix
@@ -10,7 +10,7 @@ in
     enableDefaultNetwork = false;
     networks.traefik.external = true;
     services.n8n.service = {
-      image = "n8nio/n8n:1.94.0";
+      image = "n8nio/n8n:1.111.0";
       container_name = "n8n";
       environment = rec {
         N8N_HIRING_BANNER_ENABLED = "false";
