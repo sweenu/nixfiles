28bb6bede1305064087b0c93230b61e3aa768b82
sweenu <contact@sweenu.xyz>
2025-10-10 11:08:03 +0200

    najdorf(obsidian-share-note): fix traefik routing

--- hosts/najdorf/obsidian-share-note.nix
+++ hosts/najdorf/obsidian-share-note.nix
@@ -1,6 +1,7 @@
 { self, config, ... }:
 
 let
+  fqdn = "notes.${config.vars.domainName}";
   obsidianShareNoteDir = "/opt/obsidian-share-note";
 in
 {
@@ -19,11 +20,22 @@ in
       ];
       networks = [ config.services.traefik.staticConfigOptions.providers.docker.network ];
       environment = {
-        BASE_WEB_URL = "https://notes.${config.vars.domainName}";
+        BASE_WEB_URL = "https://${fqdn}";
       };
       env_file = [ config.age.secrets."obsidian-share-note/envFile".path ];
+      healthcheck = {
+        test = [
+          "CMD-SHELL"
+          "(wget -qO - http://localhost:3000/v1/ping | grep -q ok) || exit 1"
+        ];
+        interval = "30s";
+        timeout = "5s";
+        retries = 2;
+        start_period = "10s";
+      };
       labels = {
         "traefik.enable" = "true";
+        "traefik.http.routers.to-obsidian-share-note.rule" = "Host(`${fqdn}`)";
         "traefik.http.routers.to-obsidian-share-note.service" = "obsidian-share-note";
         "traefik.http.services.obsidian-share-note.loadbalancer.server.port" = "3000";
       };
