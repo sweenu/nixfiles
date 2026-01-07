3d2fb8c7dbf685fb806b2590030ccace3aafb079
sweenu <contact@sweenu.xyz>
2026-01-07 10:47:31 +0100

    backlight: use range rather than limit

--- pkgs/backlight.py
+++ pkgs/backlight.py
@@ -31,32 +31,38 @@ def _set_brightness(value: int) -> None:
 
 def change_backlight(
     action: str,
-    limit: int = 10,
+    range_start: int = 10,
+    range_end: int = 20,
     small_step: int = 1,
     big_step: int = 5,
 ) -> None:
     """Increases or decreases brightness.
 
     Takes 'dec' or 'inc' as parameter for 'action'.
-    Goes `small_step` percent at a time from 1 to `limit` percent then
-    `big_step` percent at a time from `limit` to 100 percent.
+    Goes `big_step` percent at a time below `range_start`,
+    `small_step` percent at a time from `range_start` to `range_end` percent,
+    then `big_step` percent at a time from `range_end` to 100 percent.
     """
     current_brightness = _get_brightness()
 
     if action == "inc":
-        if current_brightness < limit:
-            if current_brightness < 1:
-                _set_brightness(1)
+        if range_start <= current_brightness < range_end:
+            _increase_brightness(small_step)
+        elif current_brightness < range_start:
+            if current_brightness + big_step > range_start:
+                _set_brightness(range_start)
             else:
-                _increase_brightness(small_step)
+                _increase_brightness(big_step)
         else:
             _increase_brightness(big_step)
     elif action == "dec":
-        if current_brightness <= limit:
-            if current_brightness > 1:
-                _decrease_brightness(small_step)
+        if range_start < current_brightness <= range_end:
+            _decrease_brightness(small_step)
+        elif current_brightness > range_end:
+            if current_brightness - big_step < range_end:
+                _set_brightness(range_end)
             else:
-                _set_brightness(0)
+                _decrease_brightness(big_step)
         else:
             _decrease_brightness(big_step)
 
