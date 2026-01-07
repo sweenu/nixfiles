069f37742cd6fb989ecf3edbff97037976331685
sweenu <contact@sweenu.xyz>
2026-01-07 12:02:11 +0100

    laptop: remove udev power rules (replace by dms)

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -16,15 +16,4 @@
   systemd.sleep.extraConfig = "HibernateDelaySec=2h";
 
   environment.defaultPackages = with pkgs; [ powertop ];
-
-  services.udev.extraRules =
-    let
-      activatePowerSaver = pkgs.writeShellScript "power-save" "${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver";
-      activatePerformance = pkgs.writeShellScript "performance" "${pkgs.power-profiles-daemon}/bin/powerprofilesctl set performance";
-    in
-    ''
-      SUBSYSTEM=="power_supply", ATTR{status}=="Discharging", RUN+="${activatePowerSaver}"
-      SUBSYSTEM=="power_supply", ATTR{status}=="Charging", RUN+="${activatePerformance}"
-      SUBSYSTEM=="power_supply", ATTR{status}=="Full", RUN+="${activatePerformance}"
-    '';
 }
