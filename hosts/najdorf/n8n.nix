4dce50ab2e4dad04d6e40e7d176f1878ec162a50
sweenu <contact@sweenu.xyz>
2026-02-14 00:59:49 +0100

    n8n: use the module's runners

--- hosts/najdorf/n8n.nix
+++ hosts/najdorf/n8n.nix
@@ -15,11 +15,11 @@ in
   age.secrets = {
     "n8n/encryptionKey".file = "${self}/secrets/n8n/encryption_key.age";
     "n8n/runnersAuthToken".file = "${self}/secrets/n8n/runners_auth_token.age";
-    "n8n/runnersEnv".file = "${self}/secrets/n8n/runners_env_file.age";
   };
 
   services.n8n = {
     enable = true;
+    taskRunners.enable = true;
     customNodes = with pkgs; [
       n8n-nodes-carbonejs
     ];
@@ -31,21 +31,8 @@ in
       N8N_VERSION_NOTIFICATIONS_ENABLED = "false";
       N8N_HIRING_BANNER_ENABLED = "false";
       N8N_DIAGNOSTICS_ENABLED = "false";
-      N8N_RUNNERS_ENABLED = "true";
-      N8N_RUNNERS_MODE = "external";
       N8N_RUNNERS_AUTH_TOKEN_FILE = config.age.secrets."n8n/runnersAuthToken".path;
     };
-  };
-
-  virtualisation.arion.projects.n8n.settings.services.n8n-runners.service = {
-    image = "n8nio/runners:${config.services.n8n.package.version}";
-    container_name = "n8n-runners";
-    network_mode = "host";
-    # https://community.n8n.io/t/support-n8n-runners-auth-token-file-in-n8nio-runners/242849
-    # environment = {
-    #   N8N_RUNNERS_AUTH_TOKEN_FILE = config.age.secrets."n8n/runnersAuthToken".path;
-    # };
-    env_file = [ config.age.secrets."n8n/runnersEnv".path ];
   };
 
   services.traefik.dynamicConfigOptions.http = rec {
