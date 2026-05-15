2e6c3f6dffe9b3b756c1ef6831db8b995268eb3a
sweenu <contact@sweenu.xyz>
2026-05-15 10:34:58 +0200

    hyprland: 0.54.2 -> 0.55.1

--- lib.nix
+++ lib.nix
@@ -1,6 +1,6 @@
 {
   hyprMoveWsToMonitor =
-    ws: "hyprctl dispatch moveworkspacetomonitor ${builtins.toString ws} $SHIKANE_OUTPUT_NAME";
+    ws: ''hyprctl dispatch "hl.dsp.workspace.move({ workspace = \"${builtins.toString ws}\", monitor = \"$SHIKANE_OUTPUT_NAME\" })"'';
   openTCPPortForLAN =
     port:
     "iptables -A nixos-fw -p tcp -s 192.168.1.0/24 --dport ${builtins.toString port} -j nixos-fw-accept";
