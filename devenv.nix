2250e76cc63135dabe920f237b956cdea16bcfde
sweenu <contact@sweenu.xyz>
2026-05-20 16:00:11 +0200

    devenv: add restic command with access to the najdorf backup

--- devenv.nix
+++ devenv.nix
@@ -25,6 +25,14 @@ in
       flakeInputs.disko.packages.${pkgs.system}.default
     ];
 
+  scripts.restic.exec = ''
+    export RESTIC_PASSWORD="$(${pkgs.rage}/bin/rage -d -i $HOME/.ssh/id_ed25519 $DEVENV_ROOT/secrets/restic/password.age)"
+    set -a
+    source <(${pkgs.rage}/bin/rage -d -i $HOME/.ssh/id_ed25519 $DEVENV_ROOT/secrets/restic/env.age)
+    set +a
+    exec ${pkgs.restic}/bin/restic "$@"
+  '';
+
   enterShell = ''
     echo ""
     echo "🚀 Welcome to nixfiles devshell"
@@ -34,6 +42,7 @@ in
     echo "  - deploy-rs: Deployment tool"
     echo "  - nixos-anywhere: Remote NixOS installation"
     echo "  - disko: Declarative disk partitioning"
+    echo "  - restic: Wrapper for the najdorf backup repository"
     echo ""
   '';
 }
